2f140d28a9bf98e7ae8b499f71f3b589ad1cad29
Iamneo <techadmin@iamneo.ai>
2023-09-07 10:54:55 +0000

    Backup Commit

--- Patterns/exception/Main.java
+++ Patterns/exception/Main.java
@@ -1,15 +1,22 @@
 package exception;
 
+import java.util.Scanner;
+
 public class Main {
-    int age = 78;
-    if(age<18 | age>150)
+    int age;
+    Scanner s = new Scanner(System.in);
+    age = s.nextInt();
+    try {
+     if(age<18 | age>150)
     {
         throw new NewException();
     }
     else
     {
         System.out.print("welcome to the billionare club");
+    }   
+    } catch (Exception e) {
+        // TODO: handle exception
+        System.out.println(e);
     }
-
-
 }
