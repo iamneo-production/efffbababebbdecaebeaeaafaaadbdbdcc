e456eaeb7945d107351cc24b207e5e33e2d2afc2
Iamneo <techadmin@iamneo.ai>
2023-09-07 09:52:23 +0000

    Backup Commit

--- Patterns/Exception.java
+++ Patterns/Exception.java
@@ -1,3 +1,10 @@
+public class invalidexcept extends Exception
+{
+    public invalidexcept(String stri)
+    {
+        super(stri);
+    }
+}
 public class Exception {
     public static void main(String[] args) {
         
