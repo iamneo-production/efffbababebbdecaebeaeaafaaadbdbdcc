35799b03f4113b6a4972aa9a5686455f29c8073f
Iamneo <techadmin@iamneo.ai>
2023-09-07 09:16:46 +0000

    Backup Commit

---
+++ Patterns/star.java
@@ -0,0 +1,10 @@
+public class star extends Thread
+{
+    public void run()
+    {
+        System.out.println("threading");
+    }
+    public static void main(String[] args) {
+        System.out.println("hello world");        
+    }
+}
