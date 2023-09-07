a97065ddd00627ab4683bcbe5f4bb6ee37c6b6eb
Iamneo <techadmin@iamneo.ai>
2023-09-07 10:47:23 +0000

    Backup Commit

--- Patterns/exception/Main.java
+++ Patterns/exception/Main.java
@@ -4,7 +4,12 @@ public class Main {
     int age = 78;
     if(age<18 | age>150)
     {
-        
+        throw new NewException();
     }
+    else
+    {
+        System.out.print("welcome to the billionare club");
+    }
+
 
 }
