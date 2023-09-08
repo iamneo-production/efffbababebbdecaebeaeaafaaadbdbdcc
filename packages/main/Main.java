cba420815f19aa08fceac6bc1117a27a00c5831b
Iamneo <techadmin@iamneo.ai>
2023-09-08 08:10:53 +0000

    Backup Commit

--- packages/main/Main.java
+++ packages/main/Main.java
@@ -4,7 +4,7 @@ import packages.code.Logic;
 
 public class Main {
     public static void main(String[] args) {
-        packages.code.Logic l = new Logic();
-        l.message1();
+        Logic lo = new Logic();
+        lo.message1();
     }   
 }
