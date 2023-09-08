d1307074f496632f1d856ee22282ec674cfcb221
Iamneo <techadmin@iamneo.ai>
2023-09-08 06:28:08 +0000

    Backup Commit

--- packages/main/Main.java
+++ packages/main/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Logic logic = new Logic();
         logic.message1();
-        logic.message2()
+        logic.message2();
     }   
 }
