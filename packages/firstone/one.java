bebf460265d34676f07bca0043c6f0d74170e3ad
Iamneo <techadmin@iamneo.ai>
2023-09-08 06:18:07 +0000

    Backup Commit

--- packages/firstone/one.java
+++ packages/firstone/one.java
@@ -3,6 +3,12 @@ package packages.firstone;
 public class one {
     public void message1()
     {
-        
+        System.out.print("if you are in ");
+        System.out.println("\"business\"");
+    }
+    public void message2()
+    {
+        System.out.print("welcome to the");
+        System.out.println("\"billionares club\"");
     }
 }
--- packages/secondone/two.java
+++ packages/secondone/two.java
@@ -1,5 +1,11 @@
 package packages.secondone;
 
+import packages.firstone.one;
+
 public class two {
-    
+    public static void main(String[] args) {
+        one obj = new one();
+        obj.message1();
+        obj.message2();
+    }
 }
