3922ca00ec80ad169b2f1eb184b225edc6f4d8b1
Iamneo <techadmin@iamneo.ai>
2023-09-08 06:08:08 +0000

    Backup Commit

--- testing/first/class1.java
+++
@@ -1,8 +0,0 @@
-public class class1
-{
-    public void msg()
-    {
-        System.out.println("first class method");
-        System.out.println("\"text in the double codes"\");
-    }
-}
--- testing/second/class2.java
+++
@@ -1,9 +0,0 @@
-package testing.second;
-package testing.first;
-public class class2 {
-    public static void main(String[] args) {
-        class1 obj = new class1();
-        obj.msg();
-
-    }
-}
