70b7683f6da67428fe92064fa8ba0d17406fa552
Iamneo <techadmin@iamneo.ai>
2023-09-12 09:54:48 +0000

    Backup Commit

--- jsevents/js/events2.js
+++ jsevents/js/events2.js
@@ -3,7 +3,7 @@ function check()
     {
         var x = document.getElementById("val").value;
         if(x == "yes")
-            window.alert("you are right")
+            document.write("you are right")
         else
-            window.alert("no you are wrong")
+            document.write("no you are wrong")
     }
