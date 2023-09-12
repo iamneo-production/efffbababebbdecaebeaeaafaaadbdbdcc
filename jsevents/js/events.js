98f3d12023c0972c39a04ef6feb5daf2d798a1e4
Iamneo <techadmin@iamneo.ai>
2023-09-12 05:35:29 +0000

    Backup Commit

---
+++ jsevents/js/events.js
@@ -0,0 +1,8 @@
+function hover()
+        {
+            document.getElementById("image").src = "https://www.icegif.com/wp-content/uploads/tom-and-jerry-icegif-17.gif";
+        }
+        function out()
+        {
+            document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png";
+        }
