95b0d14a5a8da71210ac476679f5a87647ac8db2
Iamneo <techadmin@iamneo.ai>
2023-09-12 05:40:21 +0000

    Backup Commit

--- jsevents/js/events.js
+++ jsevents/js/events.js
@@ -1,8 +1,12 @@
 function hover()
-        {
-            document.getElementById("image").src = "https://www.icegif.com/wp-content/uploads/tom-and-jerry-icegif-17.gif";
-        }
-        function out()
-        {
-            document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png";
-        }
+    {
+        document.getElementById("image").src = "https://www.icegif.com/wp-content/uploads/tom-and-jerry-icegif-17.gif";
+    }
+function out()
+    {
+        document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png";
+    }
+function show()
+    {
+        window.write()
+    }
