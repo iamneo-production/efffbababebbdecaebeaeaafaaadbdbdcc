2e7184019127261b932240d9a23524f80c295875
Iamneo <techadmin@iamneo.ai>
2023-09-12 06:20:21 +0000

    Backup Commit

--- jsevents/js/events2.js
+++ jsevents/js/events2.js
@@ -2,22 +2,20 @@ let fir = window.confirm("are you a prgrammer")
     if(fir == true)
     {
         window.alert("hello")
-           document.getElementById("ans").innerHTML = "answer the below the question";
-           document.getElementById("ques").innerHTML ="System.out.println(123456789);";
-
-        }
+        document.getElementById("ans").innerHTML = "answer the below the question";
+        document.getElementById("ques").innerHTML ="System.out.println(123456789);";
+    }
+    else
+    {
+        document.getElementById("val").style.visibility = "hidden";
+        document.getElementById("button1").style.visibility = "hidden";
+        document.getElementById("ans").innerHTML = "Thank You";
+    }
+function check()
+    {
+        var x = document.getElementById("val").value;
+        if(x == "yes")
+            window.alert("you are right")
         else
-        {
-            document.getElementById("val").style.visibility = "hidden";
-            document.getElementById("button1").style.visibility = "hidden";
-            document.getElementById("ans").innerHTML = "Thank You";
-        }
-        function check()
-        {
-            
-            var x = document.getElementById("val").value;
-            if(x == "yes")
-                window.alert("you are right")
-            else
-                window.alert("no you are wrong")
-        }
+            window.alert("no you are wrong")
+    }
