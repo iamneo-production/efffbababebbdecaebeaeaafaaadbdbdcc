a197118a70dc46ca80d16c77a6d32876c8f2b991
Iamneo <techadmin@iamneo.ai>
2023-09-12 05:50:21 +0000

    Backup Commit

--- jsevents/js/events.js
+++ jsevents/js/events.js
@@ -1,12 +1,22 @@
+
+document.getElementById("load").addEventListener("onload",loading())
+document.getElementById("load").addEventListener("onmouseover",hover())
+document.getElementById("work").addEventListener("onclick",show())
+let a=0;
+function loading()
+{
+    a = window.prompt("Enter a number")
+}
 function hover()
-    {
-        document.getElementById("image").src = "https://www.icegif.com/wp-content/uploads/tom-and-jerry-icegif-17.gif";
-    }
+{
+    document.getElementById("image").src = "https://www.icegif.com/wp-content/uploads/tom-and-jerry-icegif-17.gif";
+}
 function out()
-    {
-        document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png";
-    }
+{
+    document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/en/2/2f/Jerry_Mouse.png";
+}
 function show()
-    {
-        window.write("hello studio");
-    }
+{
+    document.write("clicked successfully");
+}
+</script>
