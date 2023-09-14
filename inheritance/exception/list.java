bb805825a676e0d1f4e3e5052bd9b15835188993
Iamneo <techadmin@iamneo.ai>
2023-09-14 06:03:48 +0000

    Backup Commit

---
+++ inheritance/exception/list.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class list {
+ public static void main(String[] args) {
+    List sam_list = new ArrayList<>();
+    Set sam_set = new HashMap<>();
+        
+ }   
+}
